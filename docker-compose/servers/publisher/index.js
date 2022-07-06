16dff487b8e7e85414013f6d6a71ca74d05877ff
JackJey <69510203+JackJey@users.noreply.github.com>
2022-07-07 04:31:38 +0900

    refuctoring

--- docker-compose/servers/publisher/index.js
+++ docker-compose/servers/publisher/index.js
@@ -1,18 +1,26 @@
-const express = require('express');
+import express from "express";
+import { readFile } from "node:fs/promises";
+
+
+const port = process.env.port || 3000;
+const host = process.env.host || 'localhost';
+
 const app = express();
 
 app.set("view engine", "ejs");
 
-app.get('/', function (req, res) {
-  const domains = [
-    "publisher.example",
+app.get('/', async (req, res) => {
+  console.log(process.env)
+  const message = `${host}: ${Date.now()}`
+  const hosts = [
     "advertizer.example",
+    "publisher.example",
     "dsp.example",
-    "ssp.example",
+    "ssp.example"
   ]
-  res.render("index", { message: `${process.env.host}: ${Date.now()}`, domains });
+  res.render("index", { message, hosts });
 });
 
-app.listen(process.env.port, function () {
-  console.log(`Listening on port ${process.env.port}`);
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
 });
