f97b67c385a25e5c0ff2f6afa5c38ed037b24a32
JackJey <69510203+JackJey@users.noreply.github.com>
2022-07-08 18:40:01 +0900

    fmt

--- docker-compose/servers/publisher/index.js
+++ docker-compose/servers/publisher/index.js
@@ -1,26 +1,19 @@
-import express from "express";
-import { readFile } from "node:fs/promises";
+import express from "express"
 
+const port = process.env.port || 3000
+const host = process.env.host || "localhost"
 
-const port = process.env.port || 3000;
-const host = process.env.host || 'localhost';
+const app = express()
 
-const app = express();
+app.set("view engine", "ejs")
 
-app.set("view engine", "ejs");
-
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   console.log(process.env)
   const message = `${host}: ${Date.now()}`
-  const hosts = [
-    "advertizer.example",
-    "publisher.example",
-    "dsp.example",
-    "ssp.example"
-  ]
-  res.render("index", { message, hosts });
-});
+  const hosts = ["advertizer.example", "publisher.example", "dsp.example", "ssp.example"]
+  res.render("index", { message, hosts })
+})
 
 app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
-});
+  console.log(`Listening on port ${port}`)
+})
