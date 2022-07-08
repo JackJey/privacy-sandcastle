78a2f186336a98b3e67ee17bb7b5966d01a764d3
JackJey <69510203+JackJey@users.noreply.github.com>
2022-07-08 23:11:23 +0900

    tsc express

--- docker-compose/servers/publisher/src/index.ts
+++ docker-compose/servers/publisher/src/index.ts
@@ -1,14 +1,14 @@
-import express from "express"
+import express, { Application, Request, Response } from "express"
 
-const port = process.env.port || 3000
+const port = process.env.port || "3000"
 const host = process.env.host || "localhost"
 
-const app = express()
+const app: Application = express()
 
 app.set("view engine", "ejs")
 app.set("views", "src/views")
 
-app.get("/", async (req, res) => {
+app.get("/", async (req: Request, res: Response) => {
   console.log(process.env)
   const message = `${host}: ${Date.now()}`
   const hosts = ["advertizer.example", "publisher.example", "dsp.example", "ssp.example"]
