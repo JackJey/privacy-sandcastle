570cc801aae41f0ee6e7341f49d6d7a6fd537211
JackJey <69510203+JackJey@users.noreply.github.com>
2022-07-09 03:31:07 +0900

    [publisher] move fledge publisher

---
+++ docker-compose/servers/publisher/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from "express"
+
+const port = process.env.port || "3000"
+const host = process.env.host || "localhost"
+
+const app: Application = express()
+
+app.use(express.static("src/public"))
+app.set("view engine", "ejs")
+app.set("views", "src/views")
+
+app.get("/", async (req: Request, res: Response) => {
+  const params = {
+    title: "FLEDGE Daily News",
+    ot_token: ""
+  }
+  res.render("index", params)
+})
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`)
+})
