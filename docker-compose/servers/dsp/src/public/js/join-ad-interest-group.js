5ac3eb47033d5ae8b2c041fabda482155b085194
JackJey <69510203+JackJey@users.noreply.github.com>
2022-07-13 02:35:27 +0900

    [dsp] up

--- docker-compose/servers/dsp/src/public/js/join-ad-interest-group.js
+++ docker-compose/servers/dsp/src/public/js/join-ad-interest-group.js
@@ -1,34 +1,33 @@
-const ads = new URL(location.href).searchParams.get('ads');
-console.log(ads);
+const ads = new URL(location.href).searchParams.get("ads")
+console.log(ads)
 
 // dsp
 const interestGroup = {
   name: ads,
-  owner: 'https://dsp-fledge-demo.glitch.me',
+  owner: "https://dsp.example",
 
   // x-allow-fledge: true
-  biddingLogicUrl: 'https://dsp-fledge-demo.glitch.me/dsp/bidding-logic.js',
+  biddingLogicUrl: "https://dsp.example/js/bidding-logic.js",
 
   // x-allow-fledge: true
-  trustedBiddingSignalsUrl:
-    'https://dsp-fledge-demo.glitch.me/dsp/bidding_signal.json',
-  trustedBiddingSignalsKeys: ['key1', 'key2'],
+  trustedBiddingSignalsUrl: "https://dsp.example/bidding_signal.json",
+  trustedBiddingSignalsKeys: ["key1", "key2"],
 
-  dailyUpdateUrl: 'https://dsp-fledge-demo.glitch.me/dsp/daily_update_url', // not implemented yets
-  userBiddingSignals: { user_bidding_signals: 'user_bidding_signals' },
+  dailyUpdateUrl: "https://dsp.example/daily_update_url", // not implemented yets
+  userBiddingSignals: { user_bidding_signals: "user_bidding_signals" },
   ads: [
     {
       renderUrl: `https://${ads}-fledge-demo.glitch.me/advertiser/${ads}-ad.html`,
       metadata: {
-        type: ads,
-      },
-    },
-  ],
-};
-console.log(interestGroup);
+        type: ads
+      }
+    }
+  ]
+}
+console.log(interestGroup)
 
-document.addEventListener('DOMContentLoaded', async (e) => {
-  console.log(e);
-  const kSecsPerDay = 3600 * 24 * 30;
-  console.log(await navigator.joinAdInterestGroup(interestGroup, kSecsPerDay));
-});
+document.addEventListener("DOMContentLoaded", async (e) => {
+  console.log(e)
+  const kSecsPerDay = 3600 * 24 * 30
+  console.log(await navigator.joinAdInterestGroup(interestGroup, kSecsPerDay))
+})
