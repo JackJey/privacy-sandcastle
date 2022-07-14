dccba41b7d283e7e5c2f07cd63451391ce1cd03d
JackJey <69510203+JackJey@users.noreply.github.com>
2022-07-15 04:43:10 +0900

    [shop] use dsp-tag

---
+++ docker-compose/servers/dsp/src/public/dsp-tag.js
@@ -0,0 +1,14 @@
+(() => {
+  const $script = document.querySelector('script.dsp_tag')
+  const advertiser = $script.dataset.advertiser
+
+  const src = new URL("https://dsp.example/join-ad-interest-group.html")
+  src.searchParams.append("ads", advertiser)
+
+  const $iframe = document.createElement('iframe')
+  $iframe.width = 1
+  $iframe.height = 1
+  $iframe.src = src
+
+  $script.parentElement.insertBefore($iframe, $script.nextSibling)
+})()
