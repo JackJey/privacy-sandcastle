5d7386320fc70c1cb9b03c2533bee3023fbf0891
JackJey <69510203+JackJey@users.noreply.github.com>
2022-07-19 22:05:17 +0900

    [shop] up ui

--- docker-compose/servers/shop/pages/index.tsx
+++ docker-compose/servers/shop/pages/index.tsx
@@ -1,24 +1,44 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
+import type { NextPage } from "next"
+import Head from "next/head"
+import Link from "next/link"
+import { items } from "../model/items"
+
+export const Item = ({ icon, name, price }: { icon: string; name: string; price: number }) => {
+  return (
+    <li className="border shadow rounded flex flex-col text-center justify-between">
+      <Link href={`/${icon}`}>
+        <a className="py-8 px-4 bg-blue-100 hover:bg-blue-50">
+          <div className="text-9xl align-tops">{icon}</div>
+          <div className="font-bold text-xl text-slate-800">{name}</div>
+          <div className="font-mono text-slate-600">${price}</div>
+        </a>
+      </Link>
+    </li>
+  )
+}
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="  bg-neutral-50 ">
       <Head>
         <title>TODO</title>
       </Head>
+      <div className="w-10/12 m-auto flex flex-col gap-6">
+        <header className="flex justify-between py-6 border-b-2">
+          <h1 className="text-4xl">Shopping DEMO</h1>
+          <Link href="/">
+            <a className="text-4xl px-2">🛒</a>
+          </Link>
+        </header>
 
-      <header>
-        <h1 className="text-8xl">
-          Shopping DEMO
-        </h1>
-      </header>
+        <main className="">
+          <ul className="grid lg:grid-cols-4 grid-cols-2 gap-4">{items.map((item) => Item(item))}</ul>
+        </main>
 
-      <main className="">main</main>
-
-      <footer className="">footer</footer>
+        <footer className="">footer</footer>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
