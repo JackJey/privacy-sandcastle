1760d10f9559a67d93a9b960c7b4642308599a1a
JackJey <69510203+JackJey@users.noreply.github.com>
2022-07-19 15:24:57 +0900

    [main] fmt

--- docker-compose/servers/dsp/src/public/dsp-tag.js
+++ docker-compose/servers/dsp/src/public/dsp-tag.js
@@ -1,11 +1,11 @@
-(() => {
-  const $script = document.querySelector('script.dsp_tag')
+;(() => {
+  const $script = document.querySelector("script.dsp_tag")
   const advertiser = $script.dataset.advertiser
 
   const src = new URL("https://dsp.example/join-ad-interest-group.html")
   src.searchParams.append("ads", advertiser)
 
-  const $iframe = document.createElement('iframe')
+  const $iframe = document.createElement("iframe")
   $iframe.width = 1
   $iframe.height = 1
   $iframe.src = src
