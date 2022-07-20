0402c67af989e39460a22a3a62fa2cc56cba5c64
JackJey <69510203+JackJey@users.noreply.github.com>
2022-07-20 10:17:35 +0900

    [shop] up

--- docker-compose/servers/shop/pages/index.tsx
+++ docker-compose/servers/shop/pages/index.tsx
@@ -26,9 +26,9 @@ const Home: NextPage = () => {
       <Head>
         <title>Shopping DEMO</title>
       </Head>
-      <div className="w-10/12 m-auto flex flex-col gap-6">
+      <div className="max-w-screen-xl p-2 lg:w-10/12 lg:p-0 lg:m-auto flex flex-col gap-6">
         <header className="flex flex-col">
-          <nav className="flex justify-between py-6 border-b-2">
+          <nav className="flex justify-between lg:py-6 py-2 border-b-2">
             <h1 className="text-slate-800 text-4xl font-bold">Shopping DEMO</h1>
             <Link href="/">
               <a className="text-4xl px-2">🛒</a>
