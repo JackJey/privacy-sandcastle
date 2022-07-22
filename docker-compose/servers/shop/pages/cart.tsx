aab93df589a6a22c546ab36efa1ad453fbfd0006
JackJey <69510203+JackJey@users.noreply.github.com>
2022-07-22 16:35:49 +0900

    [shop] up

--- docker-compose/servers/shop/pages/cart.tsx
+++ docker-compose/servers/shop/pages/cart.tsx
@@ -17,7 +17,6 @@ export const getServerSideProps = withSessionSsr(async function getServerSidePro
     const item = getItem(id)
     return { item, size, quantity }
   })
-  console.log({ orders })
   return {
     props: {
       orders
@@ -25,38 +24,39 @@ export const getServerSideProps = withSessionSsr(async function getServerSidePro
   }
 })
 
-const CartItem = (order: Order) => {
+const CartItem = ({ order }: { order: Order }) => {
   const { item, size, quantity } = order
+  const range = Array(5).fill(0)
+
   return (
-    <li key={item.id} className="grid grid-cols-3 gap-4 border">
-      <div className="flex flex-col gap-4 justify-center bg-slate-200">
+    <li key={item.id} className="grid grid-cols-3 gap-8 border">
+      <div className="col-span-1 flex flex-col gap-4  bg-slate-200">
         <Image src={`/image/svg/emoji_u${item.id}.svg`} width={100} height={100} alt={item.name}></Image>
-        <h2 className="font-bold text-xl text-center">{item.name}</h2>
+        <h2 className="font-bold text-xl text-center text-slate-700 pb-4">{item.name}</h2>
       </div>
-      <div className="col-span-2 flex flex-col justify-center gap-4 text-xl">
-        <div className="">price: ${item.price}.00</div>
-        <div>size: {size}</div>
-        <div>
-          quantity:
-          <select id="quantity" name="quantity" className="text-slate-800">
-            {Array(5)
-              .fill(0)
-              .map((_, i) => {
-                if (i === quantity) {
-                  return (
-                    <option key={i} value={i} selected>
-                      {i}
-                    </option>
-                  )
-                }
-                return (
+      <div className="col-span-2 flex flex-col justify-center gap-2 text-lg text-slate-700">
+        <dl className="flex flex-col">
+          <div className="flex gap-4">
+            <dt className="w-16 font-bold">price:</dt>
+            <dd>${item.price}.00</dd>
+          </div>
+          <div className="flex gap-4">
+            <dt className="w-16 font-bold">size:</dt>
+            <dd>{size}</dd>
+          </div>
+          <div className="flex gap-4">
+            <dt className="w-16 font-bold">quantity:</dt>
+            <dd>
+              <select id="quantity" name="quantity" className="text-slate-800" defaultValue={quantity}>
+                {range.map((_, i) => (
                   <option key={i} value={i}>
                     {i}
                   </option>
-                )
-              })}
-          </select>
-        </div>
+                ))}
+              </select>
+            </dd>
+          </div>
+        </dl>
       </div>
     </li>
   )
@@ -74,14 +74,22 @@ const Cart = ({ orders }: { orders: Array<Order> }) => {
       <div className="flex flex-col gap-6">
         <header className="flex flex-col">
           <nav className="flex justify-between lg:py-6 py-2 border-b-2">
-            <h1 className="text-slate-800 text-4xl font-bold">Shopping DEMO</h1>
+            <h1 className="text-slate-800 text-4xl font-bold">
+              <Link href="/">
+                <a>Shopping DEMO</a>
+              </Link>
+            </h1>
             <Link href="/cart">
               <a className="text-4xl px-2">🛒</a>
             </Link>
           </nav>
         </header>
 
-        <ul className="flex flex-col gap-6">{orders.map((order) => CartItem(order))}</ul>
+        <ul className="flex flex-col gap-6">
+          {orders.map((order) => (
+            <CartItem key={order.item.id} order={order} />
+          ))}
+        </ul>
 
         <dl className="flex flex-col text-2xl">
           <div className="flex justify-end gap-2">
@@ -94,7 +102,7 @@ const Cart = ({ orders }: { orders: Array<Order> }) => {
           </div>
           <div className="flex justify-end gap-2">
             <dt className="font-bold">Total:</dt>
-            <dd>${shipping + shipping}.00</dd>
+            <dd>${subtotal + shipping}.00</dd>
           </div>
         </dl>
         <footer className="border-t-2 py-4">
