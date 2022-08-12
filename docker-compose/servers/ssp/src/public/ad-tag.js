1db837d98a32dcfbe24b3e7085726349b27d4f22
JackJey <69510203+JackJey@users.noreply.github.com>
2022-08-13 01:34:10 +0900

    [ssp] fixup ad-tag

--- docker-compose/servers/ssp/src/public/ad-tag.js
+++ docker-compose/servers/ssp/src/public/ad-tag.js
@@ -2,7 +2,6 @@
   const $iframe = document.createElement("iframe")
   $iframe.width = 300
   $iframe.src = "https://ssp.example/ad-tag.html"
-
   const $script = document.querySelector("script.ssp_tag")
   $script.parentElement.insertBefore($iframe, $script.nextSibling)
 })()
