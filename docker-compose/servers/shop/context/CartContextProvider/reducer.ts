f41765c1a3f8b0127ea177e8943e2924361516bb
JackJey <69510203+JackJey@users.noreply.github.com>
2022-10-20 01:44:06 +0900

    [cart] refuctoring

--- docker-compose/servers/shop/context/CartContextProvider/reducer.ts
+++ docker-compose/servers/shop/context/CartContextProvider/reducer.ts
@@ -1,23 +1,5 @@
 import { Order } from "../../lib/items"
 
-export const ADD_ORDER = "ADD_ORDER"
-export const REMOVE_ORDER = "REMOVE_ORDER"
-export const UPDATE_ORDER = "UPDATE_ORDER"
-
-export type CartAction =
-  | {
-      type: "ADD_ORDER"
-      order: Order
-    }
-  | {
-      type: "REMOVE_ORDER"
-      order: Order
-    }
-  | {
-      type: "UPDATE_ORDER"
-      order: Order
-    }
-
 export const addOrder = (order: Order, state: Order[]) => {
   const index = state.findIndex(({ item, size }) => {
     return order.item.id === item.id && order.size === size
@@ -46,17 +28,4 @@ export const updateOrder = (order: Order, state: Order[]) => {
     }
     return [...acc, o]
   }, [])
-}
-
-export const cartReducer = (state: Order[], action: CartAction) => {
-  switch (action.type) {
-    case ADD_ORDER:
-      return addOrder(action.order, state)
-    case REMOVE_ORDER:
-      return removeOrder(action.order, state)
-    case UPDATE_ORDER:
-      return updateOrder(action.order, state)
-    default:
-      return state
-  }
 }
