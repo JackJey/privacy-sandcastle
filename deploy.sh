ddd4c8006be428b733b943df941d6195f8586d88
JackJey <69510203+JackJey@users.noreply.github.com>
2022-11-10 03:11:18 +0900

    [deploy] add cloud run/firebase deploy resouces

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,29 @@
 #/usr/bin/env zsh
 
-docker push gcr.io/privacy-sandcastle/sandcastle_home
-gcloud run deploy sandcastle-home --image gcr.io/privacy-sandcastle/sandcastle_home:latest --platform managed --region asia-northeast1
+hosts=$(cat <<EOT
+  home
+  news
+  shop
+  travel
+  dsp
+  ssp
+EOT
+)
+
+for host in $hosts; do
+  echo https://privacy-sandcastle-${host}.web.app/
+  # GCP
+  ## change gcp project
+  gcloud config set project privacy-sandcastle-${host}
+  gcloud config get-value project
+  ## push docker image
+  docker push gcr.io/privacy-sandcastle-${host}/sandcastle_${host}
+  ## deploy cloud run
+  gcloud run deploy sandcastle-${host} --image gcr.io/privacy-sandcastle-${host}/sandcastle_${host}:latest --platform managed --region asia-northeast1
+
+  # Firebase Hosting
+  ## change project
+  firebase use ${host}
+  ## deploy
+  firebase deploy
+done
