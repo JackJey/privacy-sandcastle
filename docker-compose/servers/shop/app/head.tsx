3c71732756bff04bb1af3dfd1261374de6eb841e
JackJey <69510203+JackJey@users.noreply.github.com>
2022-11-17 23:12:28 +0900

    [shop] up layout

--- docker-compose/servers/shop/app/head.tsx
+++ docker-compose/servers/shop/app/head.tsx
@@ -1,7 +1,15 @@
-export default async function Head() {
+type Props = {
+  title?: string
+}
+
+export default function Head({ title = "Shopping DEMO" }: Props) {
   return (
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>{title}</title>
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👟</text></svg>"
+      ></link>
     </>
   )
 }
--- docker-compose/servers/shop/app/layout.tsx
+++ docker-compose/servers/shop/app/layout.tsx
@@ -1,9 +1,28 @@
+import Link from "next/link"
 import "../styles/globals.css"
+
+const Header = () => {
+  return (
+    <header className="flex flex-col">
+      <nav className="flex justify-between lg:py-6 py-2 border-b-2">
+        <h1 className="text-slate-800 text-4xl font-bold">
+          <Link href="/">Shopping DEMO</Link>
+        </h1>
+        <Link href="/cart" className="text-4xl px-2">
+          🛒
+        </Link>
+      </nav>
+    </header>
+  )
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
