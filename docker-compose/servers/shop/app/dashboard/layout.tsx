02ec68b8db8f262dc075b1b55932946f8a54eddf
JackJey <69510203+JackJey@users.noreply.github.com>
2022-11-17 19:02:57 +0900

    [shop] enable tailwind in app

--- docker-compose/servers/shop/app/dashboard/layout.tsx
+++ docker-compose/servers/shop/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <header>header</header>
-      <main>{children}</main>
+      <main className="text-green-500">{children}</main>
       <footer>footer</footer>
     </div>
   )
