8cfcc13d7b494cf3b75b85c342bc08f89ae2e96f
JackJey <69510203+JackJey@users.noreply.github.com>
2023-02-15 10:51:44 +0900

    [global] fmt

--- containers/servers/news/tailwind.config.cjs
+++ containers/servers/news/tailwind.config.cjs
@@ -22,7 +22,7 @@ module.exports = {
       fontFamily: {
         serif: ["Cambria", "Cochin", "Georgia", "Times", "Times New Roman", "serif"]
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
--- containers/servers/shop/tailwind.config.js
+++ containers/servers/shop/tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontSize: {
-        'icon': '20rem'
+        icon: "20rem"
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
