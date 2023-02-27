183bf1295c0f1854c7ba2091dfb340d4c0de7a6e
JackJey <69510203+JackJey@users.noreply.github.com>
2023-02-28 06:30:32 +0900

    [news] up

--- containers/servers/news/src/index.ts
+++ containers/servers/news/src/index.ts
@@ -16,13 +16,12 @@
 
 import express, { Application, Request, Response } from "express"
 
-const port = process.env.INTERNAL_PORT as string
-const host = process.env.NEWS_HOST as string
-const token = process.env.NEWS_TOKEN as string
+const { PORT, INTERNAL_PORT, HOME_HOST, SSP_HOST, NEWS_TOKEN, NEWS_DETAIL } = process.env
+
 const app: Application = express()
 
 app.use((req, res, next) => {
-  res.setHeader("Origin-Trial", token)
+  res.setHeader("Origin-Trial", NEWS_TOKEN as string)
   next()
 })
 app.use(express.static("src/public"))
@@ -30,22 +29,21 @@ app.set("view engine", "ejs")
 app.set("views", "src/views")
 
 app.get("/", async (req: Request, res: Response) => {
-  const home = new URL(`https://${process.env.HOME_HOST}:${process.env.PORT}`)
-  const ad_tag = new URL(`https://${process.env.SSP_HOST}:${process.env.PORT}/ad-tag.js`)
-  const title = process.env.NEWS_DETAIL
+  const title = NEWS_DETAIL
   const lorem =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
   const params = {
     title,
-    token,
     lorem,
-    home,
-    ad_tag
+    PORT,
+    HOME_HOST,
+    NEWS_TOKEN,
+    SSP_HOST
   }
   res.render("index", params)
 })
 
-app.listen(port, async () => {
-  console.log(`Listening on port ${port}`)
+app.listen(INTERNAL_PORT, async () => {
+  console.log(`Listening on port ${INTERNAL_PORT}`)
 })
