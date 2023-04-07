6721bdbbb48baae605e8201ab5158a9dabd4bc88
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2023-04-07 22:10:51 +0900

    [scripts] move and breakdown deployment scripts - break down initial setup and deployment scripts - split firebase and cloud run script so they can be used independently

---
+++ scripts/firebase_deploy.sh
@@ -0,0 +1,43 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#/usr/bin/env zsh
+
+# parameters
+firebase_hosting_domain="privacy-sandcastle"; # replace with your hosting site domain
+
+services=$(cat <<EOT
+  home
+  news
+  shop
+  travel
+  dsp
+  ssp
+EOT
+)
+
+# Deploy to Firebase hosting all sites
+for service in $services; do
+  firebase deploy --only hosting:${service}
+done
+
+# Firebase Hosting : check all sites
+for service in $services; do
+  printf https://${firebase_hosting_domain}-${service}.web.app/
+
+  ## cleanup cache
+  curl -X PURGE https://${firebase_hosting_domain}-${service}.web.app/
+
+  printf "\n"
+done
