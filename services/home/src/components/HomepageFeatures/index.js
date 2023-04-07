88e58b2f2b2f47a595df5ec0889dbb85d510e8e7
JackJey <69510203+JackJey@users.noreply.github.com>
2023-04-07 23:05:51 +0900

    [home] update docs

--- services/home/src/components/HomepageFeatures/index.js
+++ services/home/src/components/HomepageFeatures/index.js
@@ -4,32 +4,32 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'DEMO of Privacy Sandbox API Families',
+    Svg: require('@site/static/img/home-hero-user.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Privacy Sandcastle is the DEMO project for
+        covering every Privacy Sandbox API in single demo.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Covering real world use-cases',
+    Svg: require('@site/static/img/padlock-information.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Privacy Sandcastle focuses on not only each API usage
+        But align with real world use-cases for combination of APIs
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Locally Runnable',
+    Svg: require('@site/static/img/shield-information.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Privacy Sandcastle make local development easy by pre configured
+        Valid domain/certificates and Origin Trials token embedding.
       </>
     ),
   },
