2c805b3a798fcaec1ff89de995e106d3e07d6c71
JackJey <69510203+JackJey@users.noreply.github.com>
2023-06-03 02:00:08 +0900

    [home] enable algolia search

--- services/home/docusaurus.config.js
+++ services/home/docusaurus.config.js
@@ -119,6 +119,35 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "30C2Z24495",
+
+        // Public API key: it is safe to commit it
+        apiKey: "fab13a7b22e907ce81ed9ef0904f4122",
+
+        indexName: "privacy-sandcastle",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "privacy-sandcastle-home\\.dev",
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: "/docs/", // or as RegExp: /\/docs\//
+          to: "/"
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search"
+
+        //... other Algolia params
       }
     })
 }
